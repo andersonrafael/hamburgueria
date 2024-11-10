185fd5027e3ac821610ef2fac9542e3a8c4d0fcd
Francisco Anderson Rafael da Silva <andersonrafael@users.noreply.github.com>
2024-11-10 19:19:54 -0300

    erro add cart modal

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const menu = document.getElementById("menu")
 const cartBtn = document.getElementById("cart-btn")
 const cartModal = document.getElementById("cart-modal")
-const cartItensConteiner = document.getElementById("cart-itens")
+const cartItemsContainer = document.getElementById("cart-items")
 const cartTotal = document.getElementById("cart-total")
 const checkoutBtn = document.getElementById("checkout-btn")
 const closeModalBtn = document.getElementById("close-modal-btn")
@@ -9,6 +9,7 @@ const cartCounter = document.getElementById("cart-count")
 const addressInput = document.getElementById("address")
 const addressWarn = document.getElementById("address-warn")
 
+let cart = [];
 
 //abrir modal do carrinho
 cartBtn.addEventListener("click", function () {
@@ -26,3 +27,56 @@ cartModal.addEventListener("click", function (event) {
 closeModalBtn.addEventListener("click", function () {
     cartModal.style.display = "none"
 })
+
+menu.addEventListener("click", function (event) {
+    //console.log(event.target)
+
+    let parentButton = event.target.closest(".add-to-cart-btn")
+    if (parentButton) {
+        const name = parentButton.getAttribute("data-name")
+        const price = parseFloat(parentButton.getAttribute("data-price"))
+
+        addToCart(name, price)
+    }
+})
+
+//Função para adcioinar ao carrinho
+function addToCart(name, price) {
+    const existeItem = cart.find(item => item.name === name)
+    if (existeItem) {
+        //Se o item ja existe aumenta apenas a quantidade +1
+        existeItem.quantity += 1;
+
+    } else {
+        cart.push({
+            name, price, quantity: 1
+        })
+    }
+
+    updateCartModal()
+
+}
+
+//Atualiza carrinho
+function updateCartModal() {
+    cartItemsContainer.innerHTML = "";
+    let total = 0;
+
+    cart.forEach(item => {
+        const cartItemsElement = document.createElement("div");
+        cartItemsElement.innerHTML = `
+        <div>
+            <div>
+                 <p>${item.name}</p>
+                 <p>${item.quantity}</p>
+                 <p>${item.price}</p>
+            </div>
+            <div>
+            <button> Remover </button>
+            </div>
+        </div> 
+        `
+        cartItemsContainer.appendChild(cart)
+    })
+
+}
